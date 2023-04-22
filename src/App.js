1410bd30c3b172b7fd3c68a3dcf9c68909dde0ae
Yurii Voytenko <why10ko@yandex.ru>
2023-04-22 15:00:00 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from "react";
-import {useState} from 'react'
+import React, {useState, useEffect} from "react";
+const url = 'https://jsonplaceholder.typicode.com/users'
 
 
-
+// Вариант 1, вызов по кнопке
 
 // export default function App() {
 
 //   const [data, setData] = useState();
 
 //   const getApiId = () => {
-//     fetch('https://jsonplaceholder.typicode.com/users')
+//     fetch(url)
 //       .then(res => res.json())
 //       .then(data => setData(data.map(item => `id = ${item.id} Name = ${item.name} \n`)))
 //   }
@@ -23,24 +23,97 @@ import {useState} from 'react'
 //   );
 // }
 
-
-
 // Вариант 2, вывод без кнопки
+
+// export default function App() {
+
+//   const [data, setData] = useState();
+  
+//     fetch(url)
+//       .then(res => res.json())
+//       .then(data => setData(data.map(item => `id = ${item.id} Name = ${item.name} \n`)))
+
+//   return (
+//     <div className="App">
+//       <h1>Вывод</h1>  
+//       <pre>{data}</pre>
+//     </div>  
+//   );
+// }
+
+// Вариант 3, перенёс .map в jsx
+
+// export default function App() {
+
+//   const [info, setData] = useState([]);
+    
+//     fetch(url)
+//       .then(res => res.json())
+//       .then(data => setData(data))
+
+//   return (
+//     <div className="App">
+//       <h1>Вывод</h1>  
+//       {info.map(item => {
+//         return (
+//           <p key={item.id}>{`id = ${item.id} Name = ${item.name}`}</p>
+//         )
+//       })}
+//     </div>  
+//   );
+// }
+
+// Вариант 4, useEffect
+
+// export default function App() {
+
+//   const [info, setData] = useState([]);
+    
+
+//   useEffect(() => {
+//     fetch(url)
+//     .then(res => res.json())
+//     .then(data => setData(data))
+//   },[])
+
+//   return (
+//     <div className="App">
+//       <h1>Вывод</h1>  
+//       {info.map(item => {
+//         return (
+//           <p key={item.id}>{`id = ${item.id} Name = ${item.name}`}</p>
+//         )
+//       })}
+//     </div>  
+//   );
+// }
+
+
+// Вариант 5, asinc await
 
 export default function App() {
 
-  const [data, setData] = useState();
-  
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(data => setData(data.map(item => `id = ${item.id} , Name = ${item.name} \n` )))
+  const [info, setData] = useState([]);
+    
+
+  useEffect(() => {
+    const getchData = async() => {
+      const res = await fetch(url);
+      const conwertObjDataFromUrl = await res.json();
+      return setData(conwertObjDataFromUrl);
+      
+    }
+    getchData()
+  },[])
 
   return (
     <div className="App">
       <h1>Вывод</h1>  
-      <pre>{data}</pre>
+      {info.map(item => {
+        return (
+          <p key={item.id}>{`id = ${item.id} Name = ${item.name}`}</p>
+        )
+      })}
     </div>  
   );
 }
-
-
